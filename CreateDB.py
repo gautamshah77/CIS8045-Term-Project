53fbbed7fe82fab56bdad4f3859117959bf50247
DarsheeMehta <darsheemehta91@gmail.com>
2017-02-23 17:21:06 -0500

    2.0
    2.0

--- CreateDB.py
+++ CreateDB.py
@@ -21,10 +21,13 @@ py2neo.authenticate("localhost:7474", "neo4j", "termproject")
 
 graph = Graph("http://localhost:7474/db/data/")
 
-review = pd.read_csv('Desktop/Airline_data_partial.csv')
+#graph.delete_all()
+
+review = pd.read_csv('C:/Users/darsh/OneDrive/Documents/MSIS/Spring Semester/MM1/Unstructured Data Management/Term Project/Airline_data_partial.csv')
 review.columns.values
 print(review['reviewdate'])
 review_index = 0
+len(review)
 
 setAuthor = set(review['authorname'])
 setAirline = set(review['airlinename'])
@@ -40,9 +43,22 @@ for j in setAirline:
     graph.create(j)
     
 
+for k in range(0, len(review)):
+    graph.run("""MATCH (au{name:review.loc[review_index, 'authorname']}),(ai{name:review.loc[review_index, 'airlinename']}) 
+                WITH au, ai
+                CREATE (au)-[:RATED{authorcountry: review.loc[review_index, 'authorcountry'], 
+                                    aircraft: review.loc[review_index, 'aircraft'],
+                                    route: review.loc[review_index, 'route'],
+                                    travellertype: review.loc[review_index, 'travellertype'],
+                                    cabin: review.loc[review_index, 'cabin'],
+                                    rating_cabinstaff: review.loc[review_index, 'rating_cabinstaff'],
+                                    rating_foodbeverage: review.loc[review_index, 'rating_foodbeverage'],
+                                    rating_inflightEnt: review.loc[review_index, 'rating_inflightEnt'],
+                                    rating_overall: review.loc[review_index, 'rating_overall'],
+                                    rating_seatcomfort: review.loc[review_index, 'rating_seatcomfort'],
+                                    rating_valuemoney: review.loc[review_index, 'rating_valuemoney'],
+                                    recommended: review.loc[review_index, 'recommended'],
+                                    reviewcontent: review.loc[review_index, 'reviewcontent'],
+                                    reviewdate: review.loc[review_index, 'reviewdate']}]->(ai)""")
+    review_index += 1
 
-'''
-for i in range(0, len(review)):
-    author
-    review_index += 1 #Increases the row index
-'''
